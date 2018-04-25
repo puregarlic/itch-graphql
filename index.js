5158fc0f0d7185ce20b82b53955425953f73c63e
Graham Barber <grahammbarber@gmail.com>
2018-04-24 21:48:19 -0700

    feat: add starting server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-"use strict"
+'use strict'
 
+/* eslint-disable */
 // Set options as a parameter, environment variable, or rc file.
-require = require("esm")(module/*, options*/)
-module.exports = require("./main.js")
+require = require('esm')(module, { cjs: true })
+module.exports = require('./main.js')
--- main.js
+++ main.js
@@ -1,2 +1,39 @@
-// ESM syntax is supported.
-export {}
+import { microGraphiql, microGraphql } from 'apollo-server-micro'
+import micro, { send } from 'micro'
+import { get, post, router } from 'microrouter'
+import { makeExecutableSchema } from 'graphql-tools'
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const graphqlHandler = microGraphql({ schema })
+const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' })
+
+const server = function (port, graphiql = true) {
+  return micro(
+    router(
+      get('/graphql', graphqlHandler),
+      post('/graphql', graphqlHandler),
+      get(
+        '/graphiql',
+        graphiql ? graphiqlHandler : (req, res) => send(res, 405, 'sorry bub')
+      ),
+      (req, res) => send(res, 404, 'whatcha lookin for (oh four)')
+    )
+  ).listen(port, () => {
+    console.log(`hmu on port ${port}`)
+  })
+}
+
+export { server }
