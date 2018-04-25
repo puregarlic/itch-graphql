6195eb7d2685211cf073ada80f86c124da0fb24d
Graham Barber <grahammbarber@gmail.com>
2018-04-24 23:40:16 -0700

    test(api): add profile test

--- test/api.js
+++ test/api.js
@@ -3,17 +3,39 @@ import { request, plugins } from 'popsicle'
 
 require('dotenv').config()
 
-const itchToken = process.env.ITCH_TOKEN
+const headers = {
+  Authorization: `Bearer ${process.env.ITCH_TOKEN}`
+}
 
 test('returns scope info', async t => {
   const res = await request({
     method: 'GET',
     url: 'https://itch.io/api/1/key/credentials/info',
-    headers: {
-      Authorization: `Bearer ${itchToken}`
-    }
+    headers
   }).use(plugins.parse('json'))
 
   t.is(res.status, 200)
   t.deepEqual(res.body, {})
+})
+
+test('returns profile info', async t => {
+  const keys = [
+    'cover_url',
+    'display_name',
+    'developer',
+    'id',
+    'url',
+    'gamer',
+    'username',
+    'press_user'
+  ].sort()
+
+  const res = await request({
+    method: 'GET',
+    url: 'https://itch.io/api/1/key/me',
+    headers
+  }).use(plugins.parse('json'))
+
+  t.is(res.status, 200)
+  t.deepEqual(Object.keys(res.body.user).sort(), keys)
 })
