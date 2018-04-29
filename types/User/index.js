31389a7596e9545151107dbe715d80b86a245a92
Graham Barber <grahammbarber@gmail.com>
2018-04-29 12:30:46 -0700

    docs(types): annotate User fields

--- types/User/index.js
+++ types/User/index.js
@@ -1,11 +1,19 @@
 const typeDefs = `type User {
+  # The user's numeric ID
   id: ID
+  # The user's username
   username: String
+  # Whether or not the account has purchased games
   gamer: Boolean
+  # The name associated with the user
   display_name: String
+  # The URL for the user's profile pic. Great for thumbnails!
   cover_url: String
+  # The user's profile page URL
   url: String
+  # Whether the user utilizes the press system
   press_user: Boolean
+  # Whether the user has uploaded projects
   developer: Boolean
 }`
 
