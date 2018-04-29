5ce24b8c80b35783e242c1f71efc13d57605719d
Graham Barber <grahammbarber@gmail.com>
2018-04-29 12:26:30 -0700

    docs(types): annotate Purchase fields

--- types/Purchase/index.js
+++ types/Purchase/index.js
@@ -1,12 +1,21 @@
 const typeDefs = `type Purchase {
+  # Numeric ID of the purchase
   id: ID
+  # Was this out of the kindness of their heart?
   donation: Boolean
+  # Email associated with the purchase
   email: String
+  # Creation timestamp
   created_at: String
+  # Service where the payment was processed
   source: String
+  # The currency used in this purchase
   currency: String
+  # The amount that the purchase was for
   price: String
+  # If there was a discount, the amount of the discount
   sale_rate: Float
+  # The numeric ID of the purchased game
   game_id: Int
 }
 `
